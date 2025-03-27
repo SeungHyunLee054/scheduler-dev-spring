a9b0ab16c2cbc2eb94a66121f71e2c205efada14
SeungHyunLee054 <yellow13lsh@naver.com>
2025-03-27 13:41:57 +0900

    init

---
+++ src/main/java/com/lsh/scheduler_dev/SchedulerDevApplication.java
@@ -0,0 +1,13 @@
+package com.lsh.scheduler_dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchedulerDevApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchedulerDevApplication.class, args);
+    }
+
+}
