d446a27a459978cee64fe41baed3c7a004e98e30
SeungHyunLee054 <yellow13lsh@naver.com>
2025-03-28 10:51:42 +0900

    Refactor : 인증방식 변경
    과제에서 요구한 것이 cookie/session을 사용하는 것 이었기 때문에 Jwt 방식을 사용하지 않고 session에 정보를 담는 방식으로 변경

--- src/main/java/com/lsh/scheduler_dev/common/security/constant/AuthorizationConstant.java
+++
@@ -1,6 +0,0 @@
-package com.lsh.scheduler_dev.common.security.constant;
-
-public final class AuthorizationConstant {
-    public static final String AUTHORIZATION_HEADER = "Authorization";
-
-}
--- src/main/java/com/lsh/scheduler_dev/module/scheduler/controller/SchedulerController.java
+++ src/main/java/com/lsh/scheduler_dev/module/scheduler/controller/SchedulerController.java
@@ -1,16 +1,16 @@
 package com.lsh.scheduler_dev.module.scheduler.controller;
 
-import com.lsh.scheduler_dev.common.security.jwt.provieder.dto.MemberAuthDto;
+import com.lsh.scheduler_dev.module.member.dto.MemberAuthDto;
 import com.lsh.scheduler_dev.module.scheduler.dto.SchedulerCreateRequestDto;
 import com.lsh.scheduler_dev.module.scheduler.dto.SchedulerResponseDto;
 import com.lsh.scheduler_dev.module.scheduler.dto.SchedulerUpdateRequestDto;
 import com.lsh.scheduler_dev.module.scheduler.service.SchedulerService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+
 
 @RestController
 @RequestMapping("/schedulers")
@@ -20,7 +20,7 @@ public class SchedulerController {
 
     @PostMapping
     public ResponseEntity<SchedulerResponseDto> create(
-            @AuthenticationPrincipal MemberAuthDto memberAuthDto,
+            @SessionAttribute(name = "Authorization") MemberAuthDto memberAuthDto,
             @RequestBody SchedulerCreateRequestDto dto
     ) {
         return ResponseEntity.ok(schedulerService.saveScheduler(memberAuthDto.getMemberId(), dto));
@@ -33,18 +33,18 @@ public class SchedulerController {
 
     @PutMapping("/{schedulerId}")
     public ResponseEntity<SchedulerResponseDto> updateScheduler(
-            @AuthenticationPrincipal MemberAuthDto memberAuthDto,
+            @SessionAttribute(name = "Authorization") MemberAuthDto memberAuthDto,
             @PathVariable Long schedulerId,
             @RequestBody SchedulerUpdateRequestDto dto
-    ){
-        return ResponseEntity.ok(schedulerService.updateScheduler(memberAuthDto.getMemberId(),schedulerId, dto));
+    ) {
+        return ResponseEntity.ok(schedulerService.updateScheduler(memberAuthDto.getMemberId(), schedulerId, dto));
     }
 
     @DeleteMapping("{schedulerId}")
     public ResponseEntity<SchedulerResponseDto> deleteScheduler(
-            @AuthenticationPrincipal MemberAuthDto memberAuthDto,
+            @SessionAttribute(name = "Authorization") MemberAuthDto memberAuthDto,
             @PathVariable Long schedulerId
-    ){
-        return ResponseEntity.ok(schedulerService.deleteScheduler(memberAuthDto.getMemberId(),schedulerId));
+    ) {
+        return ResponseEntity.ok(schedulerService.deleteScheduler(memberAuthDto.getMemberId(), schedulerId));
     }
 }
